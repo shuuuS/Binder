1162ebc955c05d023feb28b68685b76a1cc28299
shuuuS <ikluzc@gmail.com>
2022-10-25 16:19:10 +0200

    Startup function

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ USER_NAME = getpass.getuser()
 def add_to_startup(file_path=""):
     if file_path == "":
         file_path = dirname(realpath(__file__))
-    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    bat_path = r'C:/Users/Kuba/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup' 
     with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
         bat_file.write(r'start "" "%s"' % file_path)
 
