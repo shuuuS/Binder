a14f127a55bb85e67390f1641e9fb04cb12de1fa
shuuuS <ikluzc@gmail.com>
2025-08-17 18:40:18 +0200

    Adding requirements.txt for used libraries

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 # PATHS TO FOLDERS
+<<<<<<< Updated upstream
 source_dir = "PATH/WHERE/YOU/SAVE/FILES"
 zips_dir = "PATH/TO/SPECIFIC_FILE"
 images_dir = "PATH/TO/SPECIFIC_FILE"
 docs_dir = "PATH/TO/SPECIFIC_FILE"
 video_dir = "PATH/TO/SPECIFIC_FILE"
 exe_dir = "PATH/TO/SPECIFIC_FILE"
+=======
+source_dir = "C:\\Users\\ikluz\\Downloads"
+zips_dir = "C:\\Users\\ikluz\\Downloads\\ZIPY"
+images_dir = "C:\\Users\\ikluz\\Downloads\\IMAGES"
+docs_dir = "C:\\Users\\ikluz\\Downloads\\PDF"
+video_dir = "C:\\Users\\ikluz\\Downloads\\WIDEO"
+exe_dir = "C:\\Users\\ikluz\\Downloads\\EXEKI"
+>>>>>>> Stashed changes
 
-# supported image types
+# image types
 image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
 
-# supported document types
-document_extensions = [".doc", ".docx", ".odt", ".xls", ".xlsx", ".txt", ".pdf"]
+# document types
+document_extensions = [".doc", ".docx", ".odt", ".xls", ".xlsx", ".txt", ".pdf", ".csv"]
 
-# supported zip/rar types
+# zip/rar types
 zip_extensions = ['.rar', ".zip"]
 
-# supported video types
-video_extensions = [".mp4", ".mov", ".mp3"]
+# video types
+video_extensions = [".mp4", ".mov"]
 
-# supported exe types
+# exe types
 exe_extensions = [".exe"]
