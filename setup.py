2a31020750f61a61c95e9b8f7d8706ae2b427d67
Kuba Kwiatkowski <ikluzc@gmail.com>
2025-09-24 09:43:24 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ file_types = {
     "soft_extensions": [".hex", ".bin"],
 }
 
-source_dir = "C:/Users/j.kwiatkowski/Downloads"
-image_dir = "C:/Users/j.kwiatkowski/Downloads/IMAGES"
-document_dir = "C:/Users/j.kwiatkowski/Downloads/DOCS"
-zip_dir = "C:/Users/j.kwiatkowski/Downloads/ZIPS"
-video_dir = "C:/Users/j.kwiatkowski/Downloads/VIDEOS"
-exe_dir = "C:/Users/j.kwiatkowski/Downloads/EXES"
-soft_dir = "C:/Users/j.kwiatkowski/Downloads/SOFTWARE"
+source_dir = "PATH_TO_DOWNLOAD_FOLDER"
+image_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
+document_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
+zip_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
+video_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
+exe_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
+soft_dir = "PATH_TO_FOLDER_WITH_SPECIFIC_EXTENSION"
 
-image_extensions = []
-document_extensions = [".doc", ".docx", ".xls", ".txt"]
+image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+document_extensions = [".doc", ".docx", ".docm", ".odt", ".xls", ".xlsx", ".xlsm", ".txt", ".pdf"]
 zip_extensions = [".rar", ".zip"]
 video_extensions = [".mp4", ".mov", ".mp3"]
 exe_extensions = [".exe"]
